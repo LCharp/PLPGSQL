f377332748214f57c056cd11e476d9a9b1d3c66c
Lucas Charpentier <lucas.charp36@gmail.com>
2019-05-15 16:08:44 +0200

    Update req3.sql

--- tp4/req3.sql
+++ tp4/req3.sql
@@ -1,15 +1,15 @@
 DROP  FUNCTION   IF EXISTS  tp4_req3() CASCADE;
+
 CREATE FUNCTION tp4_req3() returns TRIGGER  AS $$
-declare
+DECLARE
 BEGIN
-	update t_theme set nb_image=nb_image+1
-	where num_theme=NEW.num_theme;
+	UPDATE t_theme SET nb_image = nb_image + 1 WHERE num_theme = NEW.num_theme;
 	return NEW;
 END
 $$ language plpgsql;
 
-create trigger test_exo3 after INSERT ON t_correspondre
+CREATE TRIGGER test_exo3 after INSERT ON t_correspondre
      FOR EACH ROW
      EXECUTE PROCEDURE tp4_req3();
 
-insert into t_correspondre(num_theme,num_image) values(1,19)
+INSERT INTO t_correspondre(num_theme,num_image) VALUES (1,19);
